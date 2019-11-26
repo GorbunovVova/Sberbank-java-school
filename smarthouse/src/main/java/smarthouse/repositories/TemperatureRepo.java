9210ca9378ae89fe918f4cf640fc0f6237517316
GorbunovVova <vovin@list.ru>
2019-11-26 08:53:20 +0300

    1

--- smarthouse/src/main/java/smarthouse/repositories/TemperatureRepo.java
+++
@@ -1,9 +0,0 @@
-package smarthouse.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import smarthouse.models.TemperatureEntity;
-
-@Repository
-public interface TemperatureRepo extends JpaRepository<TemperatureEntity, Long> {
-}
--- smarthouse/src/main/java/smarthouse/repositories/WetnessRepo.java
+++
@@ -1,9 +0,0 @@
-package smarthouse.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import smarthouse.models.WetnessEntity;
-
-@Repository
-public interface WetnessRepo extends JpaRepository<WetnessEntity, Long> {
-}
