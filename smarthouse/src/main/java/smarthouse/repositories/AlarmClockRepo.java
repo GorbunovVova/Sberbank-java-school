9f7f442de80572cf7a7abf3ae3eb3203e13077f4
GorbunovVova <vovin@list.ru>
2019-12-04 16:31:32 +0300

    smarthouse is deleted

--- smarthouse/src/main/java/smarthouse/repositories/AlarmClockRepo.java
+++
@@ -1,9 +0,0 @@
-package smarthouse.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import smarthouse.models.AlarmClockEntity;
-
-@Repository
-public interface AlarmClockRepo extends JpaRepository<AlarmClockEntity, Long> {
-}
