13a80e592f772c9656e32322aedefd92a2793640
Gorbunov Vova <vovin@list.ru>
2019-12-05 14:34:32 +0300

    deleted old version

--- smarthouse/src/main/java/smarthouse/repositories/GasRepo.java
+++
@@ -1,9 +0,0 @@
-package smarthouse.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import smarthouse.models.GasEntity;
-
-@Repository
-public interface GasRepo extends JpaRepository<GasEntity, Long> {
-}
--- smarthouse/src/main/java/smarthouse/repositories/LightRepo.java
+++
@@ -1,9 +0,0 @@
-package smarthouse.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import smarthouse.models.LightEntity;
-
-@Repository
-public interface LightRepo extends JpaRepository<LightEntity, Long> {
-}
